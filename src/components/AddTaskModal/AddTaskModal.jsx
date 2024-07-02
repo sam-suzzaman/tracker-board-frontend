fc25fb8bec8786cacb39fbbf301456fbeb13fef8
sam-suzzaman <dksamsuzzaman@gmail.com>
2024-07-02 20:46:07 +0600

    add form validation

--- src/components/AddTaskModal/AddTaskModal.jsx
+++ src/components/AddTaskModal/AddTaskModal.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import Modal from "react-responsive-modal";
 
+import { useForm } from "react-hook-form";
+
 const AddTaskModal = ({ open, onClose }) => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
+
     return (
         <Modal
             open={open}
@@ -17,7 +27,11 @@ const AddTaskModal = ({ open, onClose }) => {
                 <h3 className="capitalize text-center text-tracker-700 font-semibold text-lg md:text-xl lg:text-2xl">
                     Add task
                 </h3>
-                <form className="mt-8">
+                <form
+                    className="mt-8"
+                    onSubmit={handleSubmit(onSubmit)}
+                    autoComplete="off"
+                >
                     {/* Title row */}
                     <div className="flex flex-col">
                         <label
@@ -30,10 +44,26 @@ const AddTaskModal = ({ open, onClose }) => {
                             type="text"
                             id="title"
                             className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                            {...register("title", {
+                                required: {
+                                    value: true,
+                                    message: "Task must have a title.",
+                                },
+                                maxLength: {
+                                    value: 100,
+                                    message: "Too long (max 100char)",
+                                },
+                                minLength: {
+                                    value: 5,
+                                    message: "Too short (min 5char)",
+                                },
+                            })}
                         />
-                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
-                            something went wrong
-                        </span>
+                        {errors?.title && (
+                            <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                                {errors?.title?.message}
+                            </span>
+                        )}
                     </div>
                     {/* desc row */}
                     <div className="flex flex-col mt-6">
@@ -47,10 +77,26 @@ const AddTaskModal = ({ open, onClose }) => {
                             type="text"
                             id="des"
                             className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                            {...register("description", {
+                                required: {
+                                    value: true,
+                                    message: "Task must have a description",
+                                },
+                                maxLength: {
+                                    value: 250,
+                                    message: "Too long (max 250char)",
+                                },
+                                minLength: {
+                                    value: 10,
+                                    message: "Too short (min 10char)",
+                                },
+                            })}
                         />
-                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
-                            something went wrong
-                        </span>
+                        {errors?.description && (
+                            <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                                {errors?.description?.message}
+                            </span>
+                        )}
                     </div>
                     {/* button */}
                     <div className="mt-6 flex justify-center items-center">
--- src/components/EditTaskModal/EditTaskModal.jsx
+++ src/components/EditTaskModal/EditTaskModal.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import Modal from "react-responsive-modal";
 
 const EditTaskModal = ({ open, onClose }) => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm();
+    const onSubmit = (data) => console.log(data);
     return (
         <Modal
             open={open}
@@ -17,7 +25,11 @@ const EditTaskModal = ({ open, onClose }) => {
                 <h3 className="capitalize text-center text-tracker-700 font-semibold text-lg md:text-xl lg:text-2xl">
                     Edit task
                 </h3>
-                <form className="mt-8">
+                <form
+                    className="mt-8"
+                    onSubmit={handleSubmit(onSubmit)}
+                    autoComplete="off"
+                >
                     {/* Title row */}
                     <div className="flex flex-col">
                         <label
@@ -30,10 +42,26 @@ const EditTaskModal = ({ open, onClose }) => {
                             type="text"
                             id="title"
                             className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                            {...register("title", {
+                                required: {
+                                    value: true,
+                                    message: "Task must have a title.",
+                                },
+                                maxLength: {
+                                    value: 100,
+                                    message: "Too long (max 100char)",
+                                },
+                                minLength: {
+                                    value: 5,
+                                    message: "Too short (min 5char)",
+                                },
+                            })}
                         />
-                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
-                            something went wrong
-                        </span>
+                        {errors?.title && (
+                            <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                                {errors?.title?.message}
+                            </span>
+                        )}
                     </div>
                     {/* desc row */}
                     <div className="flex flex-col mt-6">
@@ -47,10 +75,26 @@ const EditTaskModal = ({ open, onClose }) => {
                             type="text"
                             id="des"
                             className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                            {...register("description", {
+                                required: {
+                                    value: true,
+                                    message: "Task must have a description",
+                                },
+                                maxLength: {
+                                    value: 250,
+                                    message: "Too long (max 250char)",
+                                },
+                                minLength: {
+                                    value: 10,
+                                    message: "Too short (min 10char)",
+                                },
+                            })}
                         />
-                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
-                            something went wrong
-                        </span>
+                        {errors?.description && (
+                            <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                                {errors?.description?.message}
+                            </span>
+                        )}
                     </div>
                     {/* Task Status row */}
                     <div className="flex flex-col mt-6">
@@ -64,15 +108,34 @@ const EditTaskModal = ({ open, onClose }) => {
                             name="status"
                             id="status"
                             className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                            defaultValue="none"
+                            {...register("status", {
+                                required: {
+                                    value: true,
+                                    message: "Task Status is required",
+                                },
+                                validate: {
+                                    valueType: (value) => {
+                                        return (
+                                            value !== "none" ||
+                                            "Task Status is required"
+                                        );
+                                    },
+                                },
+                            })}
                         >
-                            <option value="default">select one option</option>
-                            <option value="in-progress">in-progress</option>
-                            <option value="complete">complete</option>
+                            <option disabled value="none">
+                                Select Option
+                            </option>
+                            <option value="in-progress">In-progress</option>
+                            <option value="complete">Complete</option>
                         </select>
 
-                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
-                            something went wrong
-                        </span>
+                        {errors?.status && (
+                            <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                                {errors?.status?.message}
+                            </span>
+                        )}
                     </div>
                     {/* button */}
                     <div className="mt-6 flex justify-center items-center">

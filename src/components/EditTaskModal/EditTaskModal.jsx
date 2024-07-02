0db8da11175e41263f39840bf80bf909bab6df9c
sam-suzzaman <dksamsuzzaman@gmail.com>
2024-07-02 20:26:34 +0600

    add all modal required to crud

--- src/components/EditTaskModal/EditTaskModal.jsx
+++ src/components/EditTaskModal/EditTaskModal.jsx
@@ -3,13 +3,88 @@ import Modal from "react-responsive-modal";
 
 const EditTaskModal = ({ open, onClose }) => {
     return (
-        <Modal open={open} onClose={onClose} center closeOnOverlayClick={false}>
-            <h2>Simple centered modal</h2>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                pulvinar risus non risus hendrerit venenatis. Pellentesque sit
-                amet hendrerit risus, sed porttitor quam.
-            </p>
+        <Modal
+            open={open}
+            onClose={onClose}
+            center
+            closeOnOverlayClick={false}
+            classNames={{
+                overlay: "editTaskModalCustomOverlay",
+                modal: "editTaskModalCustomModal",
+            }}
+        >
+            <div className="">
+                <h3 className="capitalize text-center text-tracker-700 font-semibold text-lg md:text-xl lg:text-2xl">
+                    Edit task
+                </h3>
+                <form className="mt-8">
+                    {/* Title row */}
+                    <div className="flex flex-col">
+                        <label
+                            htmlFor="title"
+                            className="capitalize text-xs font-semibold inline-block mb-2"
+                        >
+                            Task title:
+                        </label>
+                        <input
+                            type="text"
+                            id="title"
+                            className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                        />
+                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                            something went wrong
+                        </span>
+                    </div>
+                    {/* desc row */}
+                    <div className="flex flex-col mt-6">
+                        <label
+                            htmlFor="des"
+                            className="capitalize text-xs font-semibold inline-block mb-2"
+                        >
+                            Task Description:
+                        </label>
+                        <textarea
+                            type="text"
+                            id="des"
+                            className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                        />
+                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                            something went wrong
+                        </span>
+                    </div>
+                    {/* Task Status row */}
+                    <div className="flex flex-col mt-6">
+                        <label
+                            htmlFor="title"
+                            className="capitalize text-xs font-semibold inline-block mb-2"
+                        >
+                            Task Status:
+                        </label>
+                        <select
+                            name="status"
+                            id="status"
+                            className="outline-none border-tracker-200 border-[1px] w-full rounded-[4px] px-[10px] py-[4px] text-sm font-semibold text-black focus:border-tracker-400 transition-all duration-300"
+                        >
+                            <option value="default">select one option</option>
+                            <option value="in-progress">in-progress</option>
+                            <option value="complete">complete</option>
+                        </select>
+
+                        <span className="capitalize text-[11px] font-semibold inline-block mt-1 text-red-400 tracking-wide">
+                            something went wrong
+                        </span>
+                    </div>
+                    {/* button */}
+                    <div className="mt-6 flex justify-center items-center">
+                        <button
+                            type="submit"
+                            className="capitalize font-semibold text-sm bg-tracker-600 text-white px-6 py-2 rounded-sm transition-all duration-300 hover:bg-tracker-800"
+                        >
+                            Update task
+                        </button>
+                    </div>
+                </form>
+            </div>
         </Modal>
     );
 };
--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,13 +1,37 @@
 import React, { useState } from "react";
 
-
 import EditTaskModal from "../EditTaskModal/EditTaskModal";
+import Swal from "sweetalert2";
 
 const Task = () => {
     // Edit Modal
     const [open, setOpen] = useState(false);
     const onOpenModal = () => setOpen(true);
     const onCloseModal = () => setOpen(false);
+
+    // delete task handler
+    const taskDeleteHandler = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "Task will be deleted permanently and can't be undo.",
+            icon: "warning",
+            showCancelButton: true,
+            cancelButtonText: "Cancel, Don't delete",
+            confirmButtonText: "Yes",
+            confirmButtonColor: "#33a440d5",
+            cancelButtonColor: "#f54d4de5",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    title: "Done",
+                    text: "Task removed successfully.",
+                    icon: "success",
+                    confirmButtonText: "Close",
+                    confirmButtonColor: "#ff0088",
+                });
+            }
+        });
+    };
 
     return (
         <div className="bg-white px-4 py-6 rounded-[4px]">
@@ -29,13 +53,18 @@ const Task = () => {
                     </button>
                 </div>
                 <div className="flex justify-end items-center gap-[6px]">
+                    {/* edit btn */}
                     <button
                         onClick={onOpenModal}
                         className="text-[12px] font-bold capitalize bg-orange-200 text-orange-800 px-2 py-1 rounded-sm tracking-wide cursor-pointer hover:bg-orange-500  hover:text-white transition-all duration-300"
                     >
                         <i class="fa-regular fa-pen-to-square"></i>
                     </button>
-                    <button className="text-[12px] font-bold capitalize bg-red-100 text-red-700 px-2 py-1 rounded-sm tracking-wide cursor-pointer hover:bg-red-400  hover:text-white transition-all duration-300">
+                    {/* delete btn */}
+                    <button
+                        onClick={taskDeleteHandler}
+                        className="text-[12px] font-bold capitalize bg-red-100 text-red-700 px-2 py-1 rounded-sm tracking-wide cursor-pointer hover:bg-red-400  hover:text-white transition-all duration-300"
+                    >
                         <i class="fa-solid fa-trash"></i>
                     </button>
                 </div>
